e1f2e8cc49fa15ad8820776398fb403a080426d8
BartoszF <felis.bartosz@gmail.com>
2016-10-15 13:22:28 +0200

    Added Node class.

--- src/pl/bartoszf/Node.java
+++ src/pl/bartoszf/Node.java
@@ -1,9 +1,28 @@
 package pl.bartoszf;
 
+import java.util.List;
+
 /**
  * Created by Kebab on 15.10.2016.
  */
 public class Node
 {
+    private Vector2 position;
+    private List<Node> connections;
 
+    public Node(Vector2 pos)
+    {
+        this.position = pos;
+    }
+
+    public void addEdge(Node n)
+    {
+        connect(n);
+        n.connect(this);
+    }
+
+    public void connect(Node n) { this.connections.add(n);}
+
+    public Vector2 getPos() {return this.position;}
+    public double dist(Node n) {return Vector2.Dist(position, n.getPos());}
 }
